210a095050ba1526b45063f1a38594a58e4dba5c
Admin <Admin@JIVIN-DIYA-PC>
2024-10-07 00:00:57 +0530

    Add project files.

---
+++ UserDefinedException/Program.cs
@@ -0,0 +1,13 @@
+﻿// See https://aka.ms/new-console-template for more information
+using UserDefinedException;
+
+Temperature temp = new();
+try
+{
+    temp.showTemp();
+}
+catch (TempIsZeroException e)
+{
+    Console.WriteLine("TempIsZeroException: {0}", e.Message);
+}
+Console.ReadKey();
